4ff5323651d847ee50d50c610ec398f93d39e3da
Felipe França <contatofelipefranca12@gmail.com>
2020-09-17 21:29:53 -0300

    Adiciona palavra

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Digite qualquer palavra: ");
+            string palavra = Console.ReadLine().ToUpper();
+            Console.WriteLine($"Palavra em CAIXA ALTA: {palavra}");
+            Console.ReadKey();
+        }
+    }
+}
